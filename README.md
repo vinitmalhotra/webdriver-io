0d77da1058c1094718e0d2b58f544114bdbda57e
Malhotra <vinit.malhotra@capgemini.com>
2020-11-16 22:26:16 +1100

    Commiting final Project

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ version  v12.13.1
 # To setup node module 
 - "npm install"
 
+# NOTE :- Always give unique email address in the Registration.feature file to begin test execution
+
 # To run the test case
--  $ ./node_modules/.bin/wdio wdio.conf.js
+- npm run test
 
-# To view  the reports
-- ./node_modules/.bin/allure generate allure-results/ --clean  && node_modules/.bin/allure open
+# For generating Allure reports   
+- npm run reports      
 
+# NOTE :- To open the report use the command. (Pelase open in google chrome as IE has inconsistencies)
+- node_modules/.bin/allure open
+
+# For Assingment purpose I didnt not add allure reports in the gitignore file.
--- step-definitions/registration.js
+++ step-definitions/registration.js
@@ -2,165 +2,94 @@ const { Given, When, Then, And } = require('cucumber');
 const chai = require('chai');
 
 Given(/^I'm on the Automation Practise landing page$/, function () {
-    //browser.url('http://automationpractice.com/index.php');
-   browser.url('http://automationpractice.com/index.php?controller=search');
-    browser.maximizeWindow();
-
-    
+  browser.url('http://automationpractice.com/index.php')
+  browser.maximizeWindow();
 });
 
 When(/^I click \"([^\"]*)\" link$/, function (link) {
-   
-    const btn = $(`//a[contains(text(),'${link}')]`)
-    btn.click();
-  });
+  const btn = $(`//a[contains(text(),'${link}')]`)
+  btn.click();
+});
 
-  When(/^I enter the email address \"([^\"]*)\"$/, function (emailid) {
+When(/^I click \"([^\"]*)\" button$/, function (value) {
+  const button = $(`//button[contains(., '${value}')]`);
+  button.click();
+  browser.pause(6000);
+});
 
-   browser.pause(6000);
-    const emailid_create = $("//input[@id='email_create']");
-    emailid_create.setValue(emailid);
-   
+When(/^I select \"([^\"]*)\" title$/, function (mr) {
+  const gender = $("//input[@id='id_gender1']");
+  gender.click();
+});
 
-  });
+When(/^I enter \"([^\"]*)\" \"([^\"]*)\"$/, function (fieldname, value,) {
+  const inputname = $(`//input[contains(@class,'is_required')][contains(@name,'${fieldname}')]`);
+  inputname.setValue(value);
+});
 
-  When(/^I click \"([^\"]*)\" button$/, function (value) {
-   // const button =   $("//button[contains(., 'Create an account')]");
-    const button =   $(`//button[contains(., '${value}')]`);
-    button.click();
-    browser.pause(6000);
+When(/^I enter the \"([^\"]*)\" \"([^\"]*)\"$/, function (fieldname, value,) {
+  const inputname = $(`//input[contains(@class,'form-control')][contains(@name,'${fieldname}')]`);
+  inputname.setValue(value);
+});
 
-  });
+When(/^I enter date \"([^\"]*)\"$/, function (date) {
+  const datebox = $('#days');
+  datebox.selectByAttribute('value', date);
+});
 
-  When(/^I select \"([^\"]*)\" title$/, function (mr) {
-    const gender = $("//input[@id='id_gender1']");
-    gender.click();
+When(/^I enter month \"([^\"]*)\"$/, function (date) {
+  const datebox = $('#months');
+  datebox.selectByAttribute('value', '3');
+});
 
-  });
+When(/^I enter year \"([^\"]*)\"$/, function (date) {
+  const datebox = $('#years');
+  datebox.selectByAttribute('value', date);
+});
 
-  When(/^I enter first name \"([^\"]*)\"$/, function (name) {
-   const firstname = $('#customer_firstname');
-   firstname.setValue(name);
-  });
+When(/^I enter state \"([^\"]*)\"$/, function (value) {
+  const statebox = $('#id_state');
+  statebox.selectByAttribute('value', '3');
+});
 
-  When(/^I enter last name \"([^\"]*)\"$/, function (name) {
-    const lastname = $('#customer_lastname');
-    lastname.setValue(name);
-   
-   });
+When(/^I enter country \"([^\"]*)\"$/, function (value) {
+  const countrybox = $('#id_country');
+  countrybox.selectByAttribute('value', '21');
+});
 
-   When(/^I enter emailid \"([^\"]*)\"$/, function (name) {
-    const emailid = $('#email');
-    emailid.setValue(name);
-   });
+Then(/^I Validate \"([^\"]*)\" on the landing screen$/, function (value) {
+  const name = $(".account>span").getText();
+  chai.assert.equal(value, name);
+});
 
-   When(/^I enter password \"([^\"]*)\"$/, function (name) {
-    const password = $('#passwd');
-    password.setValue(name);
-  });
+When(/^I search a product \"([^\"]*)\"$/, function (value) {
+  const searchbox = $('#search_query_top');
+  searchbox.setValue(value);
+});
 
-  When(/^I enter date \"([^\"]*)\"$/, function (date) {
-   const datebox = $('#days');
-   datebox.selectByAttribute('value',date);
-    //browser.pause(6000)
+When(/^I move to \"([^\"]*)\" button$/, function (value) {
+  const elem = $(`//span[contains(., '${value}')]`);
+  elem.scrollIntoView();
+  elem.moveTo();
+});
 
-  });
+When(/^I click the \"([^\"]*)\" button$/, function (value) {
+  browser.pause(6000);
+  const button = $(`//span[contains(., '${value}')]`);
+  button.click();
+});
 
-  When(/^I enter month \"([^\"]*)\"$/, function (date) {
-    const datebox = $('#months');
-    datebox.selectByAttribute('value','3');
+When(/^I navigate by clicking \"([^\"]*)\" button$/, function (valule) {
+  const button = $(".cart_navigation > .button-medium");
+  button.click();
+});
 
-   // datebox.selectByVisibleText(date);
- 
-   });
+When(/^I agree TC$/, function () {
+  const checkbox = $("//input[@name='cgv']");
+  checkbox.click();
+});
 
-   When(/^I enter year \"([^\"]*)\"$/, function (date) {
-    const datebox = $('#years');
-    datebox.selectByAttribute('value',date);
- 
-   });
-
-   
-   When(/^I enter address \"([^\"]*)\"$/, function (value) {
-    const address = $('#address1');
-    address.setValue(value);
- 
-   });
-
-   When(/^I enter city \"([^\"]*)\"$/, function (value) {
-    const city = $('#city');
-    city.setValue(value);
-   });
-
-   When(/^I enter state \"([^\"]*)\"$/, function (value) {
-        const statebox = $('#id_state');
-        statebox.selectByAttribute('value','3');
-     
-       });
-
-       When(/^I enter postcode \"([^\"]*)\"$/, function (value) {
-        const postcode = $('#postcode');
-        postcode.setValue(value);
-       });
-
-       When(/^I enter country \"([^\"]*)\"$/, function (value) {
-        const countrybox = $('#id_country');
-        countrybox.selectByAttribute('value','21');
-      });
-      When(/^I enter mobilephone \"([^\"]*)\"$/, function (value) {
-        const mobilephone = $('#phone_mobile');
-        mobilephone.setValue(value);
-      });
-
-     Then(/^I Validate \"([^\"]*)\" on the landing screen$/, function (value) {
-       // const name = $("//a[@title='View my customer account']/span").getText();
-        const name = $(".account>span").getText();
-        
-
-        chai.assert.equal(value,name);
-      });
-
-      When(/^I search a product \"([^\"]*)\"$/, function (value) {
-        const searchbox = $('#search_query_top');
-        searchbox.setValue(value);
-      });
-
-      When(/^I move to \"([^\"]*)\" button$/, function (value) {
-        const elem = $(`//span[contains(., '${value}')]`);
-        elem.scrollIntoView();
-        elem.moveTo();
-      // $("//span[contains(., 'In stock')]").moveTo();
-       browser.pause(6000)
-
-      });
-
-
-      When(/^I click the \"([^\"]*)\" button$/, function (value) {
-        const button =   $(`//span[contains(., '${value}')]`);
-        button.click();
-        browser.pause(6000);
-      });
-
-      When(/^I navigate by clicking \"([^\"]*)\" button$/, function (valule) {
-        const button =   $(".cart_navigation > .button-medium");
-        button.click();
-    });
-
-      When(/^I agree TC$/, function () {
-        browser.pause(4000);
-        const checkbox = $("//input[@name='cgv']");
-      
-        checkbox.click();
-       
-      });
-
-  Then(/^I Validate \"([^\"]*)\" on the payments page$/, function (value) {
-    const name = $(".cart_description>.product-name>a").getText();
-        
-
-    chai.assert.equal(value,name);
-      });
-
-     
-    
-    
+Then(/^I Validate \"([^\"]*)\" on the payments page$/, function (value) {
+  const name = $(".cart_description>.product-name>a").getText();
+  chai.assert.equal(value, name);
+});
